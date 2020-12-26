6def32184e837ca471edca3f42386f16eb9eab6c
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-12-26 14:46:46 -0600

    better pronunciation

--- logic/messages.js
+++ logic/messages.js
@@ -1,11 +1,11 @@
 module.exports = {
   Fajr: 'Time to pray Fajr.',
   PreSunrise: '25 minutes left to pray Fajr.',
-  Dhuhr: 'Time to pray Duh-her.',
-  PreAsr: '25 minutes left to pray Duh-her.',
-  Asr: 'Time to pray Asr.',
-  PreMaghrib: '25 minutes left to pray Asr.',
+  Dhuhr: 'Time to pray Dohar.',
+  PreAsr: '25 minutes left to pray Dohar.',
+  Asr: 'Time to pray Asar.',
+  PreMaghrib: '25 minutes left to pray Asar.',
   Maghrib: 'Time to pray Maghrib.',
   PreIsha: '25 minutes left to pray Maghrib.',
-  Isha: 'Time to pray Isha.'
+  Isha: 'Time to pray Eesha.'
 };
